801808c48e47536765e5b1d907f274ca36e356f9
Aeden McClain <dev@platypro.net>
2025-06-19 10:46:47 -0700

    Add existing website

---
+++ prism/index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<HTML>
+<HEAD>
+<meta charset="UTF-8">
+<link rel="icon" href="/favicon.png" />
+<link rel="stylesheet" type="text/css" href="/style.css" />
+<STYLE>
+.button 
+{
+	background-color:#0000DD;
+	color:#FFFFFF;
+	font-size:25px;
+	margin-bottom:15px;
+	padding:5px 15px;
+	display:block;
+	text-align:center;
+}
+
+.button:hover
+{
+	background-color: #3333DD;
+}
+
+.button:active
+{
+	background-color: #000033;	
+}
+
+.textarea
+{
+	font-size:20px;
+	text-align:center;
+	font-weight:bold;
+	color:black;
+}
+
+h2
+{
+	font-size:30px;
+	text-align:center;
+	font-weight:bold;
+	color:#505050;
+}
+
+</STYLE>
+<TITLE>Prism: The Maze Game</TITLE>
+</HEAD>
+<BODY>
+  <div class="StripBar Container">
+    <span style="text-align: left;display: table-cell;">
+    <a href="/">Home</a> -> Pri'sm: The Maze Game
+    </span>
+    <span style="text-align: right;display: table-cell;">
+    Aeden McClain 2018
+    </span>
+  </div>
+
+	<DIV style="width:30%;min-width: 500px;margin-top:15px;margin-left:auto;margin-right:auto" align="center">
+		<p style="font-size:28px;margin:0;text-align:right;font-style:italic;">The Maze Game</p>
+		<IMG src="title.png" style="display:block;margin-bottom:0px;padding:0px;width:100%" alt="<<Pri'sm Logo>>"/>
+		<DIV style="margin-top:0px;background-color:white;padding:1px 10px">
+			<h2>What is this?</h2>
+			
+			<DIV class="textarea" style="margin-top:5px">
+				"Prism: The Maze Game" is a version of capture the flag played in a user-defined room, which can be changed using lua. 
+			</DIV>
+
+			<DIV class="textarea" style="margin-top:15px">
+				More Info on the game can be found in the included readme.
+			</DIV>
+
+			<h2>Downloads:</h2>
+			
+			<a href="https://static.platypro.net/dl/software/prism/alpha_02/prism_alpha_02_win32.zip">
+			<span class="button">
+				Download windows 32bit<br>
+			</span>
+			</a>
+
+			<a href="https://static.platypro.net/dl/software/prism/alpha_02/prism_alpha_02_tux64.tar.xz">
+			<span class="button">
+				Download linux 64bit<br>
+			</span>
+			</a>
+			
+			<a href="https://static.platypro.net/dl/software/prism/alpha_02/prism_alpha_02_src.tar.xz">
+			<span class="button">
+				Download source<br>
+			</span>
+			</a>
+
+			<h2>Other Stuff:</h2>
+
+			<a href="https://platypro.itch.io/prism">
+			<span class="button">
+				itch.io page<br>
+			</span>
+			</a>
+
+			<a href="https://gitlab.com/platypro/prism">
+			<span class="button">
+				GitLab<br>
+			</span>
+			</a>
+
+			<span class="button">
+				?<br>
+			</span>
+
+			<DIV class="textarea" style="margin-top:5px">
+				<em>Current Version: alpha_02</em>
+			</DIV>
+		</DIV>
+	</DIV>
+</BODY>
+</HTML>
